78bdc374eb387d59712e759cddccf3273b72b6f3
cjyfff <1318434601@qq.com>
2016-04-04 17:46:36 +0800

    add plotROC

--- ml/ada_boost/adaboost.py
+++ ml/ada_boost/adaboost.py
@@ -39,12 +39,47 @@ def ada_boost_train_ds(data_arr, class_labels, num_lt=40):
         print "total error: %s\n" % error_rate
         if error_rate == 0.0:
             break
-    return weak_class_arr
+    return weak_class_arr, agg_class_est
+
+
+def plotROC(pred_strengths, class_labels):
+    """
+    从右上角（1.0, 1.0）开始遍历，遍历的顺序为按预测值
+    有小到大开始，预测值小的，但是被归为1的，说明真阳率下降，向y轴负方向移动，
+    预测值小的，被归为0（或-1）的，说明假阴率下降，向x轴负方向移动
+    """
+    import matplotlib.pyplot as plt
+    cur = (1.0, 1.0)
+    y_sum = 0.0
+    num_pos_clas = (np.array(class_labels) == 1.0).sum()
+    y_step = 1 / float(num_pos_clas)
+    x_step = 1 / float(len(class_labels) - num_pos_clas)
+    sorted_indicies = pred_strengths.argsort()
+    fig = plt.figure()
+    fig.clf()
+    ax = plt.subplot(111)
+    for index in sorted_indicies.tolist()[0]:
+        if class_labels[index] == 1.0:
+            del_x = 0
+            del_y = y_step
+        else:
+            del_x = x_step
+            del_y = 0
+            y_sum += cur[1]
+        ax.plot([cur[0], cur[0] - del_x], [cur[1], cur[1] - del_y], c='b')
+        cur = (cur[0] - del_x, cur[1] - del_y)
+    ax.plot([0, 1], [0, 1], 'b--')
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('ROC curve for AdaBoost Horse Colic Detection System')
+    ax.axis([0, 1, 0, 1])
+    print "the Area Under the Curve is: ", y_sum * x_step
+    plt.show()
 
 
 if __name__ == '__main__':
     data_mat, class_labels = load_simp_data()
-    classifier_array = ada_boost_train_ds(data_mat, class_labels, 9)
+    classifier_array, agg_class_est = ada_boost_train_ds(data_mat, class_labels, 9)
     """
     classifier_array是保存弱分类器的数组，格式如下：
     [{'dim': 0, 'ineq': 'lt', 'thresh': 1.3, 'alpha': 0.6931471805599453},
@@ -52,3 +87,7 @@ if __name__ == '__main__':
      {'dim': 0, 'ineq': 'lt', 'thresh': 0.90000000000000002, 'alpha': 0.8958797346140273}]
     """
     print('classifier_array:', classifier_array)
+    """
+    agg_class_est是没有被二值化的预测结果，值的绝对值越大，表示置信度越高
+    """
+    plotROC(agg_class_est.T, class_labels)
