cc7f4d4debd7b9caf0e20282efd0c0d4524378a7
cjyfff <1318434601@qq.com>
2017-07-07 17:22:44 +0800

    库存数据保存到redis中

--- highly_concurrent_stock_demo/src/main/java/com/example/demo/service/ConcurrentAccess.java
+++ highly_concurrent_stock_demo/src/main/java/com/example/demo/service/ConcurrentAccess.java
@@ -2,21 +2,29 @@ package com.example.demo.service;
 
 import java.math.BigDecimal;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import org.springframework.stereotype.Service;
 
 /**
- * Created by jiashen on 17-7-6.
+ *
+ * @author jiashen
+ * @date 17-7-6
  */
 @Service
 public class ConcurrentAccess {
 
     public ConcurrentAccess(WorkFlow workFlow) {
-        ExecutorService es = Executors.newFixedThreadPool(100);
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+            .setNameFormat("demo-pool-%d").build();
+        ExecutorService es = new ThreadPoolExecutor(10, 100, 0L,
+            TimeUnit.MILLISECONDS, new LinkedBlockingQueue<>(), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 100000; i++) {
             es.submit(() -> {
                 try {
                     workFlow.count(1, BigDecimal.valueOf(-1));
@@ -30,7 +38,7 @@ public class ConcurrentAccess {
         try {
             while (true) {
                 if (es.isTerminated()) {
-                    workFlow.getStock();
+                    workFlow.getStock(1);
                     break;
                 }
                 TimeUnit.MILLISECONDS.sleep(100);
--- highly_concurrent_stock_demo/src/main/java/com/example/demo/service/WorkFlow.java
+++ highly_concurrent_stock_demo/src/main/java/com/example/demo/service/WorkFlow.java
@@ -1,15 +1,9 @@
 package com.example.demo.service;
 
 import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Map;
 
-import com.example.demo.mapper.StockMapper;
-import com.example.demo.mapper.StockOperateLogMapper;
-import com.example.demo.model.Stock;
-import com.example.demo.model.StockOperateLog;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -22,31 +16,27 @@ public class WorkFlow {
     @Autowired
     WriteStockOperateLog writeStockOperateLog;
 
-    // id与库存的对应关系
-    private Map<Integer, BigDecimal> stockMap;
+    private RedisTemplate<Object, Object> redisTemplate;
 
-    public WorkFlow() {
+    public WorkFlow(RedisTemplate<Object, Object> redisTemplate) {
     //    模拟一开始把数据库中的id，库存关系加载到缓存
-        stockMap = new HashMap<Integer, BigDecimal>() {
-            {
-                put(1, BigDecimal.valueOf(1000000000));
-            }
-        };
+        this.redisTemplate = redisTemplate;
+        this.redisTemplate.opsForValue().set(1, BigDecimal.valueOf(1000000000));
         System.out.println("WorkFlow init completed.");
     }
 
     public synchronized void count(Integer stockId, BigDecimal operateAmount) throws Throwable {
-            BigDecimal nowAmount = stockMap.get(stockId);
+            BigDecimal nowAmount = (BigDecimal) this.redisTemplate.opsForValue().get(stockId);
             BigDecimal newAmount = nowAmount.add(operateAmount);
             if (Integer.valueOf(1).equals(newAmount.compareTo(BigDecimal.valueOf(0)))) {
-                stockMap.put(stockId, newAmount);
+                this.redisTemplate.opsForValue().set(stockId, newAmount);
                 writeStockOperateLog.writeStockOperateLog(stockId, operateAmount);
             } else {
                 throw new Exception("库存id： " + stockId + ", 库存不足");
             }
     }
 
-    public void getStock() {
-        System.out.println(stockMap);
+    public void getStock(Integer stockId) {
+        System.out.println(this.redisTemplate.opsForValue().get(stockId));
     }
 }
