ef1535f2da380b1ef5c1936e71722a6446681808
cjyfff <1318434601@qq.com>
2018-02-09 23:18:13 +0800

    add netty_test

---
+++ netty_test/netty_test_01/netty_test_01_client/src/main/java/com/cjyfff/client/EchoClient.java
@@ -0,0 +1,49 @@
+package com.cjyfff.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by cjyfff on 18-2-9.
+ */
+public class EchoClient {
+
+    public EchoClient() {
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();                //1
+            b.group(group)                                //2
+                    .channel(NioSocketChannel.class)            //3
+                    .remoteAddress(new InetSocketAddress("127.0.0.1", 8888))    //4
+                    .handler(new ChannelInitializer<SocketChannel>() {    //5
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast(
+                                    new EchoClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect().sync();        //6
+
+            f.channel().closeFuture().sync();            //7
+        } finally {
+            group.shutdownGracefully().sync();            //8
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        new EchoClient().start();
+    }
+}
---
+++ netty_test/netty_test_01/netty_test_01_server/src/main/java/com/cjyfff/server/EchoServer.java
@@ -0,0 +1,46 @@
+package com.cjyfff.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
+/**
+ * Created by cjyfff on 18-2-9.
+ */
+public class EchoServer {
+
+    public EchoServer() {
+    }
+    public static void main(String[] args) throws Exception {
+        new EchoServer().start();                //2
+    }
+
+    public void start() throws Exception {
+        NioEventLoopGroup group = new NioEventLoopGroup(); //3
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group)                                //4
+                    .channel(NioServerSocketChannel.class)        //5
+                    .localAddress(new InetSocketAddress(8888))    //6
+                    .childHandler(new ChannelInitializer<SocketChannel>() { //7
+                        @Override
+                        public void initChannel(SocketChannel ch)
+                                throws Exception {
+                            ch.pipeline().addLast(
+                                    new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.bind().sync();            //8
+            System.out.println(EchoServer.class.getName() + " started and listen on " + f.channel().localAddress());
+            f.channel().closeFuture().sync();            //9
+        } finally {
+            group.shutdownGracefully().sync();            //10
+        }
+    }
+}
