f5a189306fea642923dbd742e7cc00be18e43b54
cjyfff <1318434601@qq.com>
2018-08-17 18:59:49 +0800

    add file

---
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.cjyfff.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by jiashen on 18-8-17.
+ */
+@RestController
+public class MessageController {
+
+    /**
+     * 接收外部消息
+     */
+    public void acceptMsg() {}
+
+    /**
+     * 接收内部转发消息
+     */
+    public void acceptInnerMsg() {}
+}
