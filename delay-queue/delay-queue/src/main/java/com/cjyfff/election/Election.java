76c73cc9f42de7b1a5efe1d76b18ce9426e86727
cjyfff <631127458@qq.com>
2018-08-19 22:11:18 +0800

    add file

--- delay-queue/delay-queue/src/main/java/com/cjyfff/election/Election.java
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/election/Election.java
@@ -1,11 +1,17 @@
 package com.cjyfff.election;
 
 import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.annotation.PostConstruct;
 
+import com.alibaba.fastjson.JSON;
+
 import com.cjyfff.repository.ZooKeeperClient;
+import com.google.common.collect.Maps;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
 import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
@@ -24,6 +30,8 @@ public class Election {
     private static final String ELECTION_PATH = "/leader";
 
     private static final String NODE_INFO_PATH = "/node_info";
+
+    private static final String SHARDING_INFO_PATH = "/sharding_info_path";
 
     private static final String CLIENT_ID = UUID.randomUUID().toString();
 
@@ -48,13 +56,43 @@ public class Election {
 
     }
 
+    /**
+     * 把自身ip作为临时节点写入zk
+     * @param client
+     * @throws Exception
+     */
     private void writeNodeInfo(CuratorFramework client) throws Exception {
         InetAddress addr = InetAddress.getLocalHost();
         String ip=addr.getHostAddress();
-
-        client.create().withMode(CreateMode.EPHEMERAL).forPath(NODE_INFO_PATH, ip.getBytes());
+        String myNodeInfoPath = NODE_INFO_PATH + "/" + ip;
+        client.create().withMode(CreateMode.EPHEMERAL).forPath(myNodeInfoPath, "".getBytes());
     }
 
+    /**
+     * master统计节点，分配node id，写入zk
+     * @param client
+     */
+    private void masterSetShardingInfo(CuratorFramework client) throws Exception {
+        List<String> nodeIpList = client.getChildren().forPath(NODE_INFO_PATH);
+        Integer nodeId = 0;
+        Map<Integer, String> shardingMap = Maps.newHashMapWithExpectedSize(15);
+        for (String nodeIp : nodeIpList) {
+            shardingMap.put(nodeId, nodeIp);
+            logger.info("Host: " + nodeIp + "get nodeId: " + nodeId);
+            nodeId ++;
+        }
+
+        client.create().withMode(CreateMode.EPHEMERAL)
+            .forPath(SHARDING_INFO_PATH,
+                JSON.toJSONString(shardingMap).getBytes());
+
+    }
+
+    /**
+     * 选举master
+     * @param client
+     * @throws Exception
+     */
     public void electLeader(CuratorFramework client) throws Exception {
         LeaderLatch leaderLatch = new LeaderLatch(client, ELECTION_PATH, "Client #" + CLIENT_ID);
 
