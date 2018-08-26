9c87d5c681bcd061f64141525e55ec9df0c69f2f
cjyfff <631127458@qq.com>
2018-08-26 23:18:34 +0800

    modify demo

--- delay-queue/delay-queue/src/main/java/com/cjyfff/demo/LeaderSelectorTest.java
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/demo/LeaderSelectorTest.java
@@ -8,6 +8,7 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
+import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
 import org.apache.curator.framework.recipes.leader.LeaderSelector;
 import org.apache.curator.framework.recipes.leader.LeaderSelectorListener;
 import org.apache.curator.framework.state.ConnectionState;
@@ -23,49 +24,41 @@ public class LeaderSelectorTest {
     private static final int COUNT = 5;
 
     public static void main(String[] args) throws Exception {
-        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.43.241:2181",
+        CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.43.48:2181",
             new ExponentialBackoffRetry(1000, 3));
         client.start();
 
-        //List<LeaderLatch> list = new ArrayList<>();
-        //
-        //for (int i = 1; i <= COUNT; i++) {
-        //    LeaderLatch example = new LeaderLatch(client, PATH, "Client #" + i);
-        //    example.start();
-        //    list.add(example);
-        //}
-        //
-        //TimeUnit.SECONDS.sleep(10);
-        //
-        //LeaderLatch leader = null;
-        //for (LeaderLatch ll : list) {
-        //    if (ll.hasLeadership()) {
-        //        leader = ll;
-        //    }
-        //    System.out.println(ll.getId() + "\t" + ll.hasLeadership());
-        //}
-        //
-        //TimeUnit.SECONDS.sleep(2);
-        //
-        //list.remove(leader);
-        //leader.close();
-        //
-        //TimeUnit.SECONDS.sleep(2);
-        //
-        //System.out.println("========================");
-        //
-        //for (LeaderLatch ll : list) {
-        //    System.out.println(ll.getId() + "\t" + ll.hasLeadership());
-        //}
-        //
-        //for (LeaderLatch ll : list) {
-        //    ll.close();
-        //}
+        LeaderLatch leaderLatch = new LeaderLatch(client, PATH, "Client #1");
 
-        LeaderLatch example = new LeaderLatch(client, PATH, "Client # 1");
-        example.start();
+        leaderLatch.addListener(new LeaderLatchListener() {
 
-        example.await();
+            @Override
+            public void isLeader() {
+                System.out.println("I am Leader");
+
+            }
+
+            @Override
+            public void notLeader() {
+                System.out.println("I am not Leader");
+
+            }
+        });
+
+
+        leaderLatch.start();
+
+
+        int a = 0;
+        while (a < 600) {
+            TimeUnit.SECONDS.sleep(1);
+            a++;
+            if (a % 10 == 0) {
+                System.out.println(a);
+            }
+        }
+
+        leaderLatch.close();
         client.close();
 
     }
--- delay-queue/delay-queue/src/main/java/com/cjyfff/demo/LeaderSelectorTest2.java
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/demo/LeaderSelectorTest2.java
@@ -1,8 +1,11 @@
 package com.cjyfff.demo;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.framework.recipes.leader.LeaderLatch;
+import org.apache.curator.framework.recipes.leader.LeaderLatchListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
 /**
@@ -14,49 +17,40 @@ public class LeaderSelectorTest2 {
         private static final int COUNT = 5;
 
         public static void main(String[] args) throws Exception {
-            CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.43.241:2181",
+            CuratorFramework client = CuratorFrameworkFactory.newClient("192.168.43.48:2181",
                 new ExponentialBackoffRetry(1000, 3));
             client.start();
 
-            //List<LeaderLatch> list = new ArrayList<>();
-            //
-            //for (int i = 1; i <= COUNT; i++) {
-            //    LeaderLatch example = new LeaderLatch(client, PATH, "Client #" + i);
-            //    example.start();
-            //    list.add(example);
-            //}
-            //
-            //TimeUnit.SECONDS.sleep(10);
-            //
-            //LeaderLatch leader = null;
-            //for (LeaderLatch ll : list) {
-            //    if (ll.hasLeadership()) {
-            //        leader = ll;
-            //    }
-            //    System.out.println(ll.getId() + "\t" + ll.hasLeadership());
-            //}
-            //
-            //TimeUnit.SECONDS.sleep(2);
-            //
-            //list.remove(leader);
-            //leader.close();
-            //
-            //TimeUnit.SECONDS.sleep(2);
-            //
-            //System.out.println("========================");
-            //
-            //for (LeaderLatch ll : list) {
-            //    System.out.println(ll.getId() + "\t" + ll.hasLeadership());
-            //}
-            //
-            //for (LeaderLatch ll : list) {
-            //    ll.close();
-            //}
+            LeaderLatch leaderLatch = new LeaderLatch(client, PATH, "Client #1");
 
-            LeaderLatch example = new LeaderLatch(client, PATH, "Client # 2");
-            example.start();
+            leaderLatch.addListener(new LeaderLatchListener() {
 
-            example.await();
+                @Override
+                public void isLeader() {
+                    System.out.println("I am Leader");
+
+                }
+
+                @Override
+                public void notLeader() {
+                    System.out.println("I am not Leader");
+
+                }
+            });
+
+
+            leaderLatch.start();
+
+            int a = 0;
+            while (a < 600) {
+                TimeUnit.SECONDS.sleep(1);
+                a++;
+                if (a % 10 == 0) {
+                    System.out.println(a);
+                }
+            }
+
+            leaderLatch.close();
             client.close();
 
         }
