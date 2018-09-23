78d1fd75af72e7cc6ff9b5bbebf6e93f16e6a38d
cjyfff <631127458@qq.com>
2018-09-23 17:14:43 +0800

    modify status

--- delay-queue/delay-queue/src/main/java/com/cjyfff/dq/task/model/DelayQueueExecLog.java
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/dq/task/model/DelayQueueExecLog.java
@@ -2,12 +2,17 @@ package com.cjyfff.dq.task.model;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DelayQueueExecLog {
     private Long id;
 
     private String taskId;
 
-    private Byte status;
+    private Integer status;
 
     private Byte sharding;
 
@@ -18,68 +23,4 @@ public class DelayQueueExecLog {
     private Long taskResultId;
 
     private Date createdAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(String taskId) {
-        this.taskId = taskId;
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public Byte getSharding() {
-        return sharding;
-    }
-
-    public void setSharding(Byte sharding) {
-        this.sharding = sharding;
-    }
-
-    public String getFunctionName() {
-        return functionName;
-    }
-
-    public void setFunctionName(String functionName) {
-        this.functionName = functionName;
-    }
-
-    public String getParams() {
-        return params;
-    }
-
-    public void setParams(String params) {
-        this.params = params;
-    }
-
-    public Long getTaskResultId() {
-        return taskResultId;
-    }
-
-    public void setTaskResultId(Long taskResultId) {
-        this.taskResultId = taskResultId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
 }
--- delay-queue/delay-queue/src/main/java/com/cjyfff/dq/task/model/DelayTask.java
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/dq/task/model/DelayTask.java
@@ -2,6 +2,11 @@ package com.cjyfff.dq.task.model;
 
 import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class DelayTask {
     private Long id;
 
@@ -11,7 +16,7 @@ public class DelayTask {
 
     private String params;
 
-    private Byte status;
+    private Integer status;
 
     private Integer retryInterval;
 
@@ -22,84 +27,4 @@ public class DelayTask {
     private Date createdAt;
 
     private Date modifiedAt;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTaskId() {
-        return taskId;
-    }
-
-    public void setTaskId(String taskId) {
-        this.taskId = taskId;
-    }
-
-    public String getFunctionName() {
-        return functionName;
-    }
-
-    public void setFunctionName(String functionName) {
-        this.functionName = functionName;
-    }
-
-    public String getParams() {
-        return params;
-    }
-
-    public void setParams(String params) {
-        this.params = params;
-    }
-
-    public Byte getStatus() {
-        return status;
-    }
-
-    public void setStatus(Byte status) {
-        this.status = status;
-    }
-
-    public Integer getRetryInterval() {
-        return retryInterval;
-    }
-
-    public void setRetryInterval(Integer retryInterval) {
-        this.retryInterval = retryInterval;
-    }
-
-    public Byte getRetryCount() {
-        return retryCount;
-    }
-
-    public void setRetryCount(Byte retryCount) {
-        this.retryCount = retryCount;
-    }
-
-    public Byte getShardingId() {
-        return shardingId;
-    }
-
-    public void setShardingId(Byte shardingId) {
-        this.shardingId = shardingId;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getModifiedAt() {
-        return modifiedAt;
-    }
-
-    public void setModifiedAt(Date modifiedAt) {
-        this.modifiedAt = modifiedAt;
-    }
 }
