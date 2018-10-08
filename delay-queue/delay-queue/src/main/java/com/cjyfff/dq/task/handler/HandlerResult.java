a2f6372a165913927a116d52c048d6176f08d965
cjyfff <1318434601@qq.com>
2018-10-08 16:42:08 +0800

    增加执行task handler的流程

---
+++ delay-queue/delay-queue/src/main/java/com/cjyfff/dq/task/handler/HandlerResult.java
@@ -0,0 +1,18 @@
+package com.cjyfff.dq.task.handler;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by jiashen on 18-10-8.
+ */
+@Getter
+@Setter
+public class HandlerResult {
+
+    private Integer status;
+
+    private String msg;
+
+    private String result;
+}
