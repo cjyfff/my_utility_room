8be1922c8142cd646e2f050411927ffb5d8158ef
cjyfff <631127458@qq.com>
2018-11-12 23:12:35 +0800

    fix bug

--- delay-queue/delay-queue/scripts/throughput_test.py
+++ delay-queue/delay-queue/scripts/throughput_test.py
@@ -1,18 +1,18 @@
 import requests
 import uuid
 
-URL = 'http://192.168.43.10:8889/dq/acceptMsg'
+URL = 'http://127.0.0.1:8889/dq/acceptMsg'
 
 
 def main():
 
-    for i in range(50000):
+    for i in range(1000):
         data = {
             "taskId": uuid.uuid4().hex,
             "functionName": "exampleHandler", "params": "{}",
             "retryCount": 0,
             "retryInterval": 0,
-            "delayTime": 3600
+            "delayTime": 200
         }
 
         requests.post(URL, json=data)
