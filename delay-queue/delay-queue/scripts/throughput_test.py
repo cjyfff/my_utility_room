992082e7eed829fe04a8cedede8ed63b5944965c
cjyfff <631127458@qq.com>
2018-12-26 11:48:15 +0800

    添加启动脚本与配置文件

--- delay-queue/delay-queue/scripts/throughput_test.py
+++ delay-queue/delay-queue/scripts/throughput_test.py
@@ -1,7 +1,7 @@
 import requests
 import uuid
 
-URL = 'http://127.0.0.1:8889/dq/acceptMsg'
+URL = 'http://10.7.247.119:9999/dq/acceptMsg'
 
 
 def main():
@@ -9,10 +9,12 @@ def main():
     for i in range(1000):
         data = {
             "taskId": uuid.uuid4().hex,
-            "functionName": "exampleHandler", "params": "{}",
+            "functionName": "exampleHandler",
+            "delayTime": 20,
+            "params": "{}",
             "retryCount": 0,
-            "retryInterval": 0,
-            "delayTime": 200
+            "retryInterval": 1,
+            "nonceStr": uuid.uuid4().hex
         }
 
         requests.post(URL, json=data)
