fe0fac31796d82cb15eb0adc97c961426e464428
cjyfff <631127458@qq.com>
2019-01-20 16:36:15 +0800

    修正缓存策略

--- redission_test/src/test/java/ReadWriteTest.java
+++ redission_test/src/test/java/ReadWriteTest.java
@@ -26,8 +26,6 @@ public class ReadWriteTest {
 
     private RedissonClient redisson;
 
-    private final static String BLANK_STRING = " ";
-
     private final static Long SHORT_EXPIRE = 30L;
 
     private final static Long LONG_EXPIRE = 60L;
@@ -130,11 +128,12 @@ public class ReadWriteTest {
 
         String value = bucket.get();
 
-        if (value == null || BLANK_STRING.equals(value)) {
+        // 缓存中的数据为null时才查库，为空字符串时直接返回
+        if (value == null) {
             value = db.get(key);
 
-            if (value == null || BLANK_STRING.equals(value)) {
-                // 一个查询返回的数据为空，不管是数据不存在，还是该数据真的是空值，我们仍然把这个空结果进行缓存，
+            if (value == null) {
+                // 一个查询返回的数据为null，不管是数据不存在，还是该数据真的是空值，我们仍然把这个空结果进行缓存，
                 // 但它的过期时间应该设置得很短，这样防止恶意查询空数据导致缓存穿透
                 bucket.set(value, SHORT_EXPIRE + (int)(1 + Math.random() * 10), TimeUnit.SECONDS);
             } else {
