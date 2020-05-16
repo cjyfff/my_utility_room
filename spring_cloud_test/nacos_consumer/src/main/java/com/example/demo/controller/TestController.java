fa51f192dd1cad0f3cc0c148f0ac5deda2c17545
cjyfff <631127458@qq.com>
2020-05-16 23:06:33 +0800

    add nacos test

---
+++ spring_cloud_test/nacos_consumer/src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class TestController {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    @GetMapping("/consumer")
+    public String test1() {
+        return restTemplate.getForObject("http://test-provider/helloNacos",String.class);
+    }
+}
