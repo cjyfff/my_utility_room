9dfacbd6ba15dd85666643083a61265e2ef1392a
cjyfff <631127458@qq.com>
2020-05-23 19:49:15 +0800

    add post logic

---
+++ spring_cloud_test/nacos_consumer/src/main/java/com/example/demo/controller/vo/OrderAutoAuditHandlerParaVo.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller.vo;
+
+/**
+ * @author cjyfff
+ * @date 2020/5/23 17:35
+ */
+public class OrderAutoAuditHandlerParaVo {
+    private String orderId;
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+}
--- spring_cloud_test/nacos_provider/src/main/java/com/example/demo/controller/TestController.java
+++ spring_cloud_test/nacos_provider/src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,8 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.controller.vo.DefaultWebApiResult;
+import com.example.demo.controller.vo.OrderAutoAuditHandlerParaVo;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
@@ -10,5 +11,11 @@ public class TestController {
     public String helloNacos(){
         System.out.println("get request");
         return "你好，nacos！";
+    }
+
+    @RequestMapping(path = "/auditOrder", method={RequestMethod.POST})
+    public DefaultWebApiResult auditOrder(@RequestBody OrderAutoAuditHandlerParaVo reqDto) {
+        System.out.println("处理订单自动客审数据:" + reqDto.getOrderId());
+        return DefaultWebApiResult.success();
     }
 }
