e93b4d04b4ece33333c650953b5162dc8a43b089
cjyfff <631127458@qq.com>
2020-06-21 21:07:23 +0800

    add sentinel test

--- spring_cloud_test/nacos_provider/src/main/java/com/example/demo/controller/TestController.java
+++ spring_cloud_test/nacos_provider/src/main/java/com/example/demo/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.example.demo.controller.vo.DefaultWebApiResult;
 import com.example.demo.controller.vo.OrderAutoAuditHandlerParaVo;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class TestController {
@@ -13,9 +16,16 @@ public class TestController {
         return "你好，nacos！";
     }
 
+    @SentinelResource(value="auditOrder")
     @RequestMapping(path = "/auditOrder", method={RequestMethod.POST})
     public DefaultWebApiResult auditOrder(@RequestBody OrderAutoAuditHandlerParaVo reqDto) {
         System.out.println("处理订单自动客审数据:" + reqDto.getOrderId());
+        // 模拟响应超时，sentinel服务降级
+        try {
+            TimeUnit.MILLISECONDS.sleep(200);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return DefaultWebApiResult.success();
     }
 }
