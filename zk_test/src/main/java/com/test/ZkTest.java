31ad1ebce967b084882f7cde4f8ded8c795e4393
cjyfff <631127458@qq.com>
2020-08-08 11:59:36 +0800

    add test

---
+++ zk_test/src/main/java/com/test/ZkTest.java
@@ -0,0 +1,62 @@
+package com.test;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author cjyfff
+ * @date 2020/8/8 11:25
+ */
+public class ZkTest {
+
+    private CuratorFramework client;
+
+    @Before
+    public void initMethod() {
+        CuratorFramework c = CuratorFrameworkFactory.newClient("192.168.3.103:2181", 30000,
+                10000, new ExponentialBackoffRetry(1000, 3));
+        c.start();
+        this.client = c;
+    }
+
+    /**
+     * 使用版本号实现自增id
+     */
+    @Test
+    public void testVersion() {
+        String path = "/test";
+
+        try {
+
+            for (int i = 0; i < 100; i++) {
+                System.out.println(next(path));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private int next(String path) throws Exception {
+        Stat stat = client.checkExists().forPath(path);
+        if (stat == null) {
+            client.create().creatingParentsIfNeeded().withMode(CreateMode.EPHEMERAL).forPath(path, new byte[0]);
+            stat = client.checkExists().forPath(path);
+        }
+
+        client.setData().forPath(path, new byte[0]);
+        return stat.getVersion();
+
+    }
+
+    @After
+    public void afterMethod() {
+        client.close();
+    }
+}
