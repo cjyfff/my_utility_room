574e74cfbbfd9998e08666a35d370c3158f10244
cjyfff <631127458@qq.com>
2020-12-06 12:17:51 +0800

    add dymic routers test

---
+++ spring_cloud_test/spring_cloud_gateway_demo/src/main/java/com/example/spring_cloud_gateway_demo/conf/DynamicRouteServiceImpl.java
@@ -0,0 +1,76 @@
+package com.example.spring_cloud_gateway_demo.conf;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Service
+public class DynamicRouteServiceImpl implements ApplicationEventPublisherAware {
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+
+    /**
+     * 发布事件
+     */
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
+
+    /**
+     * 删除路由
+     * @param id
+     * @return
+     */
+    public String delete(String id) {
+        try {
+            log.info("gateway delete route id {}",id);
+            this.routeDefinitionWriter.delete(Mono.just(id));
+            return "delete success";
+        } catch (Exception e) {
+            return "delete fail";
+        }
+    }
+    /**
+     * 更新路由
+     * @param definition
+     * @return
+     */
+    public String update(RouteDefinition definition) {
+        try {
+            log.info("gateway update route {}",definition);
+            this.routeDefinitionWriter.delete(Mono.just(definition.getId()));
+        } catch (Exception e) {
+            return "update fail,not find route  routeId: "+definition.getId();
+        }
+        try {
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            this.publisher.publishEvent(new RefreshRoutesEvent(this));
+            return "success";
+        } catch (Exception e) {
+            return "update route fail";
+        }
+    }
+
+    /**
+     * 增加路由
+     * @param definition
+     * @return
+     */
+    public String add(RouteDefinition definition) {
+        log.info("gateway add route {}",definition);
+        routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+        return "success";
+    }
+}
--- spring_cloud_test/spring_cloud_gateway_demo/src/main/java/com/example/spring_cloud_gateway_demo/conf/GatewayRoutes.java
+++ spring_cloud_test/spring_cloud_gateway_demo/src/main/java/com/example/spring_cloud_gateway_demo/conf/GatewayRoutes.java
@@ -9,19 +9,19 @@ import org.springframework.http.HttpStatus;
 /**
  * Created by jiashen on 2020/5/20.
  */
-@Configuration
-public class GatewayRoutes {
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return builder.routes()
-            .route("test1", r -> r.path("/test1")
-                .filters(f -> f.stripPrefix(1))
-//                .retry(3)
-//                    .setStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-//                    .setStatus(HttpStatus.BAD_REQUEST))
-                .uri("http://127.0.0.1:18083/consumer")
-                .order(1))
-
-            .build();
-    }
-}
+//@Configuration
+//public class GatewayRoutes {
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//            .route("test1", r -> r.path("/test1")
+//                .filters(f -> f.stripPrefix(1))
+////                .retry(3)
+////                    .setStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+////                    .setStatus(HttpStatus.BAD_REQUEST))
+//                .uri("http://127.0.0.1:18083/consumer")
+//                .order(1))
+//
+//            .build();
+//    }
+//}
